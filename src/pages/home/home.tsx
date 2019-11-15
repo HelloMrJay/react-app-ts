5d2291595a2c62f30eb3496f13a2e092ecef4af0
loupengju <34649676@qq.com>
2019-11-15 18:45:55 +0800

    feat(webui): refine menu config

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -12,32 +12,42 @@ const Home: React.FC = () => {
   const history = useHistory();
   const [collapsed, setCollapsed] = useState(false);
   const onClick = (params: any) => {
-    if (params.key === '2') {
+    if (params.key === '0') {
       history.push('/home/userlist')
     }
   };
 
   useEffect(() => {
     document.title = 'react'
-  })
+  });
+
+  const menuItems = [
+    {
+      name: '用户列表',
+      icon: 'user'
+    },
+    {
+      name: '视频列表',
+      icon: 'video-camera'
+    },
+    {
+      name: '下载列表',
+      icon: 'video-camera'
+    }
+  ];
 
   return (
     <Layout className="a-layout-container">
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo" />
         <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={onClick}>
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span>用户列表</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span>视频列表</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="upload" />
-            <span>下载列表</span>
-          </Menu.Item>
+          {
+            menuItems.map((itm, idx) => {
+              return (
+                <Menu.Item key={idx}><Icon type={itm.icon} /><span>{itm.name}</span></Menu.Item>
+              )
+            })
+          }
         </Menu>
       </Sider>
       <Layout>
