508f8f2cd58555de3a68fd5b2b0048b279896080
loupengju <34649676@qq.com>
2019-12-09 17:09:39 +0800

    update

---
+++ src/layout/BasicLayout.tsx
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import menuItems from "../config/menu-config";
+import {Layout, Menu, Icon} from 'antd';
+import { useHistory } from 'react-router-dom';
+import './index.scss';
+
+const {Header, Sider, Content} = Layout;
+
+interface LayoutProps {
+  children?: React.ReactNode
+};
+
+const BasicLayout: React.FC<LayoutProps> = (props) => {
+  const history = useHistory();
+  const [collapsed, setCollapsed] = useState(false);
+  const onClick = (params: any) => {
+    if (params.key === '0') {
+      history.push('/home/userlist')
+    }
+  };
+
+  useEffect(() => {
+    history.location.pathname === '/home' && history.push('/home/userlist');
+    document.title = 'react'
+  });
+
+  return (
+    <Layout className="a-layout-container ignore">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo"/>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']} onClick={onClick}>
+          {
+            menuItems.map((itm, idx) => {
+              return (
+                <Menu.Item key={idx}><Icon type={itm.icon}/><span>{itm.name}</span></Menu.Item>
+              )
+            })
+          }
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{background: '#fff', padding: 0}}>
+          <Icon
+            className="trigger"
+            type={collapsed ? 'menu-unfold' : 'menu-fold'}
+            onClick={() => setCollapsed(!collapsed)}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            background: '#fff',
+            minHeight: 280,
+          }}
+        >
+          {props.children}
+        </Content>
+      </Layout>
+    </Layout>
+  )
+};
+
+export default BasicLayout;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,63 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import UserList from '../userList/userList';
-import menuItems from "../../config/menu-config";
-import './home.css';
-import {Layout, Menu, Icon} from 'antd';
-import {
-  Route,
-  useHistory
-} from 'react-router-dom';
-
-const {Header, Sider, Content} = Layout;
+import './home.scss';
+import { Route } from 'react-router-dom';
+import BasicLayout from '../../layout/BasicLayout'
 
 const Home: React.FC = () => {
-  const history = useHistory();
-  const [collapsed, setCollapsed] = useState(false);
-  const onClick = (params: any) => {
-    if (params.key === '0') {
-      history.push('/home/userlist')
-    }
-  };
-
-  useEffect(() => {
-    history.location.pathname === '/home' && history.push('/home/userlist');
-    document.title = 'react'
-  });
-
   return (
-    <Layout className="a-layout-container">
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo"/>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']} onClick={onClick}>
-          {
-            menuItems.map((itm, idx) => {
-              return (
-                <Menu.Item key={idx}><Icon type={itm.icon}/><span>{itm.name}</span></Menu.Item>
-              )
-            })
-          }
-        </Menu>
-      </Sider>
-      <Layout>
-        <Header style={{background: '#fff', padding: 0}}>
-          <Icon
-            className="trigger ignore"
-            type={collapsed ? 'menu-unfold' : 'menu-fold'}
-            onClick={() => setCollapsed(!collapsed)}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            background: '#fff',
-            minHeight: 280,
-          }}
-        >
-          <Route path="/home/userlist" component={UserList}></Route>
-        </Content>
-      </Layout>
-    </Layout>
+    <BasicLayout>
+      <Route path="/home/userlist" component={UserList}></Route>
+    </BasicLayout>
   )
 };
 
